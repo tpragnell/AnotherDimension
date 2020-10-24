999e117ea7e0067008b3ac13a8e59d0845468dab
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-10-24 11:56:57 -0400

    Included Tester and ArrayOps

---
+++ ArrayOps.java
@@ -0,0 +1,18 @@
+public class ArrayOps{
+
+  public static int sum(int[] arr){
+    int sum = 0;
+    int len = arr.length;
+    for(int i=0; i<len; i++){
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+
+
+
+
+
+
+}
---
+++ Tester.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.lang.Math;
+
+public class Tester{
+
+
+  public static void main(String[] args) {
+    // Test Method "sum"
+    int[] array = {1,2,3};
+    System.out.println( "The sum of 1,2,3 is " + ArrayOps.sum(array));
+  }
+}
