340a99e8e5e1902d65bf67fc395c512131324c7c
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-10-24 12:49:35 -0400

    Added sumRows method

--- ArrayOps.java
+++ ArrayOps.java
@@ -12,16 +12,24 @@ public class ArrayOps{
   public static int largest(int[] arr){
     int len = arr.length;
     int largest = arr[0];
-    for(int i=0; i<len; i++){
+    for(int i=1; i<len; i++){
       if(arr[i] > largest)
         largest = arr[i];
     }
     return largest;
   }
 
+  public static int[] sumRows(int[][] matrix){
+    int rowsLen = matrix.length;
+    int[] sums = new int[rowsLen];
+    for(int i=0; i<rowsLen; i++){
+      int colLen = matrix[i].length;
+      for(int k=0; k<colLen; k++){
+        sums[i] += matrix[i][k];
+      }
+    }
+    return sums;
+  }
 
-
-
-
-
+  
 }
--- Tester.java
+++ Tester.java
@@ -5,16 +5,24 @@ public class Tester{
 
 
   public static void main(String[] args) {
-    int len = args.length;
-    int[] array = new int[len];
-    for(int i=0; i<len; i++){
-      array[i] = Integer.parseInt(args[i]);
-    }
+    // int len = args.length;
+    // int[] b = new int[len];
+    // for(int i=0; i<len; i++){
+    //   b[i] = Integer.parseInt(args[i]);
+    // }
+    int[][] a = {{  1,  0, 12, -1 },
+                 {  7, -2,  2,  1 },
+                 { -5, -2,  2, -9 }};
+    int[] b = { 1, 3 ,5 };
 
     // Test Method "sum"
-    System.out.println( "The sum is " + ArrayOps.sum(array));
+    System.out.println( "The sum is " + ArrayOps.sum(b));
 
     // Test Method "largest"
-    System.out.println( "The largest value is " + ArrayOps.largest(array));
+    System.out.println( "The largest value is " + ArrayOps.largest(b));
+
+    //Test Method "sumRows"
+    System.out.println( "The sum of the rows is " + Arrays.toString(ArrayOps.sumRows(a)));
+
   }
 }
